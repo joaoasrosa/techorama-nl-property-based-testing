2ab5f12a63940ab410d74ae0775e1bcd066a9e2e
João Rosa <joaoasrosa@gmail.com>
2018-09-30 16:47:06 +0200

    Refactor

--- src/PropertyBasedTesting.Domain/PostalService.cs
+++ src/PropertyBasedTesting.Domain/PostalService.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace PropertyBasedTesting.Domain
 {
     public class PostalService
     {
+        private readonly FreeShipment _freeShipment;
+
+        public PostalService(FreeShipment freeShipment)
+        {
+            _freeShipment = freeShipment ?? throw new ArgumentNullException(nameof(freeShipment));
+        }
+        
         public bool IsFreeShipment(Parcel parcel)
         {
             return parcel.TotalPrice >= 20;
