e4279c993421d23846a2dcdce8ccc938be322e3f
João Rosa <joaoasrosa@gmail.com>
2018-09-30 16:55:37 +0200

    Organize tests

--- tests/PropertyBasedTesting.Tests.Unit/RegularTesting/WhenCalculatingParcelShipment.cs
+++ tests/PropertyBasedTesting.Tests.Unit/RegularTesting/WhenCalculatingParcelShipment.cs
@@ -8,63 +8,69 @@ namespace PropertyBasedTesting.Tests.Unit.RegularTesting
 {
     public class WhenCalculatingParcelShipment
     {
-//        [Fact]
-//        public void GivenParcelPriceIsBelow20Euros_ParcelShipmentIsNotFree()
-//        {
-//            const decimal price = 5;
-//
-//            var postalService = new PostalService();
-//            var isFreeShipment = postalService.IsFreeShipment(price);
-//
-//            Check.That(isFreeShipment).IsFalse();
-//        }
-
-        [Theory]
-        [InlineData(3)]
-        [InlineData(8)]
-        [InlineData(9)]
-        public void GivenParcelPriceIsBelow20Euros_ParcelShipmentIsNotFree(decimal price)
+        public class SingleDataPoint
         {
-            var postalService = new PostalService(new FreeShipment());
-            var isFreeShipment = postalService.IsFreeShipment(price);
-
-            Check.That(isFreeShipment).IsFalse();
-        }
-
-        [Fact]
-        public void GivenParcelPriceIsEqualOrAbove20Euros_ParcelShipmentFree()
-        {
-            const decimal price = 20;
-
-            var postalService = new PostalService(new FreeShipment());
-            var isFreeShipment = postalService.IsFreeShipment(price);
-
-            Check.That(isFreeShipment).IsTrue();
-        }
-    }
-
-    public class Foo
-    {
-        [Theory]
-        [ClassData(typeof(ParcelData))]
-        public void GivenParcelPriceIsBelow20Euros_ParcelShipmentIsNotFree(Parcel parcel)
-        {
-            var postalService = new PostalService(new FreeShipment());
-            var isFreeShipment = postalService.IsFreeShipment(parcel);
-
-            Check.That(isFreeShipment).IsFalse();
-        }
-
-        private class ParcelData : IEnumerable<object[]>
-        {
-            public IEnumerator<object[]> GetEnumerator()
+            [Fact]
+            public void GivenParcelPriceIsBelow20Euros_ParcelShipmentIsNotFree()
             {
-                yield return new object[] {new Parcel(new[] {new Item(3)})};
-                yield return new object[] {new Parcel(new[] {new Item(3), new Item(3)})};
-                yield return new object[] {new Parcel(new[] {new Item(3), new Item(3), new Item(1)})};
+                const decimal price = 5;
+
+                var postalService = new PostalService(new FreeShipment());
+                var isFreeShipment = postalService.IsFreeShipment(price);
+
+                Check.That(isFreeShipment).IsFalse();
             }
 
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+            [Fact]
+            public void GivenParcelPriceIsEqualOrAbove20Euros_ParcelShipmentFree()
+            {
+                const decimal price = 20;
+
+                var postalService = new PostalService(new FreeShipment());
+                var isFreeShipment = postalService.IsFreeShipment(price);
+
+                Check.That(isFreeShipment).IsTrue();
+            }
+        }
+
+        public class InlineData
+        {
+            [Theory]
+            [InlineData(3)]
+            [InlineData(8)]
+            [InlineData(9)]
+            public void GivenParcelPriceIsBelow20Euros_ParcelShipmentIsNotFree(decimal price)
+            {
+                var postalService = new PostalService(new FreeShipment());
+                var isFreeShipment = postalService.IsFreeShipment(price);
+
+                Check.That(isFreeShipment).IsFalse();
+            }
+        }
+        
+        public class ClassData
+        {
+            [Theory]
+            [ClassData(typeof(ParcelData))]
+            public void GivenParcelPriceIsBelow20Euros_ParcelShipmentIsNotFree(Parcel parcel)
+            {
+                var postalService = new PostalService(new FreeShipment());
+                var isFreeShipment = postalService.IsFreeShipment(parcel);
+
+                Check.That(isFreeShipment).IsFalse();
+            }
+
+            private class ParcelData : IEnumerable<object[]>
+            {
+                public IEnumerator<object[]> GetEnumerator()
+                {
+                    yield return new object[] {new Parcel(new[] {new Item(3)})};
+                    yield return new object[] {new Parcel(new[] {new Item(3), new Item(3)})};
+                    yield return new object[] {new Parcel(new[] {new Item(3), new Item(3), new Item(1)})};
+                }
+
+                IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+            }
         }
     }
 }
