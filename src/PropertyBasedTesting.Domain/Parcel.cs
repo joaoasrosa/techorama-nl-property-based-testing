15a6a262f614023214b5ff6ca674bfb476942a12
João Rosa <joaoasrosa@gmail.com>
2018-09-30 17:18:46 +0200

    Add method

--- src/PropertyBasedTesting.Domain/Parcel.cs
+++ src/PropertyBasedTesting.Domain/Parcel.cs
@@ -5,12 +5,22 @@ namespace PropertyBasedTesting.Domain
 {
     public class Parcel
     {
-        private readonly IEnumerable<Item> _items;
+        private readonly IList<Item> _items;
         public Price TotalPrice => (Price)_items.Sum(x => x.Price);
 
+        public Parcel()
+        {
+            _items = new List<Item>();
+        }
+        
         public Parcel(IEnumerable<Item> items)
         {
-            _items = items;
+            _items = items.ToList();
+        }
+
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
         }
     }
 }
